d93c670d568b2d057b849397c61e923f7c19373e
vince-destoeck <vincent@data-business.fr>
2014-05-13 13:22:45 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,5 @@
 data <- read.table("household_power_consumption.txt", sep= ";", header = TRUE, na.strings ="?")
 
-data$Date_Time <- apply(data[,c(1,2)], 1, paste, collapse=" ")
-data$Date_time <- strptime(data$Date_Time, format= "%d/%m/%Y %H:%M:%S")
---
 data$Date_Time <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 
 sub2 <- subset(data, data$Date_time >= as.POSIXct("2007-02-01 00:00:00")& data$Date_time < as.POSIXct("2007-02-03 00:00:00"))
